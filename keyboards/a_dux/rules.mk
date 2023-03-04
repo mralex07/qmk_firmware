d7f86163963ab668e68ff44cb18f94d736a0c04b
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 01:11:24 +0200

    Update rules.mk

--- keyboards/a_dux/rules.mk
+++ keyboards/a_dux/rules.mk
@@ -1,6 +1,10 @@
 # Build Options
 #   change yes to no to disable
 #
+# RP2040-specific options
+ALLOW_WARNINGS = yes
+PICO_INTRINSICS_ENABLED = no # ATM Unsupported by ChibiOS.
+
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
@@ -12,3 +16,5 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 UNICODE_ENABLE = yes        # Unicode
 AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes        # Use shared split_common code
+
+SERIAL_DRIVER = vendor
