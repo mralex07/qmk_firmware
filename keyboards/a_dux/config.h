f30a42925abfb7ec48337f9e01e21164ff4b73df
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 01:13:13 +0200

    Update config.h

--- keyboards/a_dux/config.h
+++ keyboards/a_dux/config.h
@@ -23,3 +23,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LOCKING_RESYNC_ENABLE
 
 #define EE_HANDS
+
+/* SPI & PMW3360 settings. 
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP22
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16 */
+
+/* Reset. */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
