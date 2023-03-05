6170011ac15d4f28bd635a69534c850ac7c43f2a
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 16:34:42 +0200

    Update rules.mk

--- keyboards/a_dux/rules.mk
+++ keyboards/a_dux/rules.mk
@@ -18,3 +18,8 @@ AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes        # Use shared split_common code
 
 SERIAL_DRIVER = vendor
+
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
+MOUSE_SHARED_EP = no # Unify multiple HID interfaces into a single Endpoint
+KEYBOARD_SHARED_EP = yes
