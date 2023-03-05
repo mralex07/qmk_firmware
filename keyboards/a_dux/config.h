dabf9e71622007d8b8554a59af6b73122328e68b
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 20:01:54 +0200

    Update config.h

--- keyboards/a_dux/config.h
+++ keyboards/a_dux/config.h
@@ -42,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X
 
 #define I2C_DRIVER I2CD1
-#define I2C0_SDA_PIN GP12
+#define I2C1_SDA_PIN GP12
 // #define I2C1_SDA_PAL_MODE 4
-#define I2C0_SCL_PIN GP13
+#define I2C1_SCL_PIN GP13
 // #define I2C1_SCL_PAL_MODE 4
