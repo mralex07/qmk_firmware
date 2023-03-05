8dcb75c5c34378267af32d308fa6a25e39463392
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-03-05 16:53:28 +0200

    Update config.h

--- keyboards/a_dux/config.h
+++ keyboards/a_dux/config.h
@@ -25,13 +25,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MASTER_LEFT
 #define SPLIT_USB_DETECT
 
-/* SPI & PMW3360 settings. 
-#define SPI_DRIVER SPID0
-#define SPI_SCK_PIN GP22
-#define SPI_MISO_PIN GP20
-#define SPI_MOSI_PIN GP23
-#define POINTING_DEVICE_CS_PIN GP16 */
-
 /* Reset. */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
@@ -49,7 +42,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X
 
 #define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN D1
+#define I2C0_SDA_PIN GP12
 // #define I2C1_SDA_PAL_MODE 4
-#define I2C1_SCL_PIN D0
+#define I2C0_SCL_PIN GP13
 // #define I2C1_SCL_PAL_MODE 4
