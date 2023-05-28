67354fa4f37dfbfe48a1401e1f1b500b3c72623e
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-05-28 18:17:33 +0000

    STMC_US = yes

--- keyboards/sofle/keymaps/keyhive_via_stemcell/rules.mk
+++ keyboards/sofle/keymaps/keyhive_via_stemcell/rules.mk
@@ -1,8 +1,10 @@
 MCU = STM32F411
+BOARD = STEMCELL
 
 # Bootloader selection
-BOOTLOADER = tinyuf2
+# BOOTLOADER = tinyuf2
 CONVERT_TO = stemcell
+STMC_US = yes
 
 VIA_ENABLE = yes
 RGBLIGHT_ENABLE = yes
