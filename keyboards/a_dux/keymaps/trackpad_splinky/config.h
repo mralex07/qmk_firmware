e2aef9158c4cd7925b9f2db04d3f11dfb5c3f89a
mralex07 <50528263+mralex07@users.noreply.github.com>
2023-05-30 15:36:37 +0000

    update config

--- keyboards/a_dux/keymaps/trackpad_splinky/config.h
+++ keyboards/a_dux/keymaps/trackpad_splinky/config.h
@@ -1,55 +1,40 @@
-/* Copyright 2018-2020 ENDO Katsuhiro <ka2hiro@curlybracket.co.jp> David Philip Barr <@davidphilipbarr> Pierre Chevalier <pierrechevalier83@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
+#undef PRODUCT
+#define PRODUCT "a_dux"
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+#define DIRECT_PINS { { GP5, GP1, GP26, GP23,  GP29}, { GP6, GP3, GP27, GP20, GP28 } , { GP7, GP4, GP0, GP22, GP21 } , { GP8, NO_PIN, GP9, NO_PIN, NO_PIN }}
+// #define DIRECT_PINS { { GP29, GP23, GP26, GP1,  GP5}, { GP28, GP20, GP27, GP3, GP6 } , { GP21, GP22, GP0, GP4, GP7 } , { GP9, GP8 }}
+// #define DIRECT_PINS_RIGHT { { GP5, GP1, GP26, GP23,  GP29}, { GP6, GP3, GP27, GP20, GP28 } , { GP7, GP4, GP0, GP22, GP21 } , { GP8, GP9 }}
 
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
 
+#undef SOFT_SERIAL_PIN
+#define SOFT_SERIAL_PIN GP2
+
+#undef I2C_DRIVER
+#define I2C_DRIVER I2CD1
+#undef I2C1_SDA_PIN
+#define I2C1_SDA_PIN GP14
+// #define I2C1_SDA_PAL_MODE 4
+#undef I2C1_SCL_PIN
+#define I2C1_SCL_PIN GP15
+// #define I2C1_SCL_PAL_MODE 4
+
+/* Handedness. */
+#undef EE_HANDS
 #define MASTER_RIGHT
+
 #define SPLIT_USB_DETECT
-
-/* Reset. */
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
-#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
-
 #define SPLIT_POINTING_ENABLE
 #define POINTING_DEVICE_RIGHT
 
 #define POINTING_DEVICE_ROTATION_90
 // #define POINTING_DEVICE_INVERT_X
 // #define POINTING_DEVICE_INVERT_Y
+#undef CIRQUE_PINNACLE_DIAMETER_MM
 #define CIRQUE_PINNACLE_DIAMETER_MM 40
 #define CIRQUE_PINNACLE_TAP_ENABLE
 #define POINTING_DEVICE_GESTURES_SCROLL_ENABLE
-// #define CIRQUE_PINNACLE_ADDR 0x2A
+#define CIRQUE_PINNACLE_ADDR 0x2A
 #define CIRQUE_PINNACLE_ATTENUATION EXTREG__TRACK_ADCCONFIG__ADC_ATTENUATE_1X
-
-#define I2C_DRIVER I2CD1
-#define I2C1_SDA_PIN GP14
-// #define I2C1_SDA_PAL_MODE 4
-#define I2C1_SCL_PIN GP15
-// #define I2C1_SCL_PAL_MODE 4
-
-/* VBUS detection. */
-#define USB_VBUS_PIN GP19
-
-/* CRC. */
-#define CRC8_USE_TABLE
-#define CRC8_OPTIMIZE_SPEED
