774059f0da01880df6042f5af625c83ea68f85b0
mralex07 <mister.alex07@google.com>
2023-10-23 23:52:56 +0300

    test

--- keyboards/bastardkb/skeletyl_3x6/splinky3/config.h
+++ keyboards/bastardkb/skeletyl_3x6/splinky3/config.h
@@ -8,8 +8,11 @@
 // #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* RGB settings. */
+#define WS2812_DI_PIN GP0
 #define RGB_DI_PIN GP0
-
+// #define WS2812_PIO_USE_PIO1
+//#define WS2812_EXTERNAL_PULLUP
+//#define SERIAL_USART_TX_PIN GP0
 /* Reset. */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
@@ -18,13 +21,20 @@
 /* VBUS detection. */
 #define USB_VBUS_PIN GP19
 
+/* RGB matrix support. */
 
+#define SPLIT_TRANSPORT_MIRROR
+#define RGB_MATRIX_LED_COUNT 42
+#define RGB_MATRIX_SPLIT { 21, 21 }
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+#define RGB_MATRIX_KEYPRESSES
 
 /* CRC.
 #define CRC8_USE_TABLE
-#define CRC8_OPTIMIZE_SPEED */
+#define CRC8_OPTIMIZE_SPEED
 
 #ifndef NOP_FUDGE
 #    define NOP_FUDGE 1  // this just pleases the compile so the above error is easier to spot
 #endif
-
+ */
